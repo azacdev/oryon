eb8a7bb941efd69c52a758c23f0a4403fa48293f
azacdev <azacdev@gmail.com>
2024-01-21 15:23:07 +0100

    chore: test verify webhook

--- app/(routes)/(checkout)/checkout/components/checkout-form.tsx
+++ app/(routes)/(checkout)/checkout/components/checkout-form.tsx
@@ -71,17 +71,19 @@ const CheckoutForm = () => {
           items: items,
         }
       );
+      
       window.location.href = data.data.authorization_url;
 
       const transactionReference = data.data.reference;
       console.log(transactionReference);
 
       // Call the verification endpoint
-      const verifyUrl = `${process.env.NEXT_PUBLIC_API_URL}/verify-payment/${transactionReference}`;
+      const verifyUrl = `${process.env.NEXT_PUBLIC_API_URL}/verify-payment/?reference=${transactionReference}`;
       const verifyResponse = await axios.get(verifyUrl);
       console.log(verifyResponse);
 
       // Handle the verification response as needed
+
       if (verifyResponse.data === "Verification successful") {
         toast("Payment completed");
         removeAll();
